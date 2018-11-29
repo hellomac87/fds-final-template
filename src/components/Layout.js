fd121d8cad0430d858598c3d72c3f4d4613a75e0
hellomac87 <hellomac87@gmail.com>
2018-11-29 14:01:35 +0900

    레이아웃 작성

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,12 +5,15 @@ class Layout extends Component {
   render() {
     return (
       <div>
-        <div style={{ background: '#000' }}>
+        <div style={{ background: '#000', textAlign: 'center' }}>
           <Link to={'/'} style={{ color: '#fff' }}>
             쇼핑몰
           </Link>
         </div>
-        {this.props.children}
+        <div style={{ margin: '0 auto', width: 500 }}>
+          {this.props.children}
+        </div>
+
         <div>저작권은 나에게 있습니다.</div>
       </div>
     );
